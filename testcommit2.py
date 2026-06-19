3a4b9ce2bd2b84505dd34095eec54d0d40fe79b2
Florencia Cabello González <tuemail.com>
2026-06-19 16:01:55 -0400

    se añade la primera parte del código entero

---
+++ testcommit2.py
@@ -0,0 +1,9 @@
+while True:
+    print("\n" + "=" * 50)
+    print("  SISTEMA DE TRAZABILIDAD DE MUESTRAS")
+    print("=" * 50)
+    print("1. Registrar nueva muestra")
+    print("2. Buscar y Gestionar Muestra")
+    print("3. Salir")
+    print("=" * 50)
+    opcion = input("Seleccione una opción: ") 
