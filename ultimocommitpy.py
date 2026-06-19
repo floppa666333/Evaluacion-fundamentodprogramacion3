fccc5ad53887b69fdd08d49865a713d7d7050c78
Florencia Cabello González <tuemail.com>
2026-06-19 16:16:39 -0400

    última parte del código

---
+++ ultimocommitpy.py
@@ -0,0 +1,6 @@
+    elif opcion == "3":
+        print("\nGracias por usar el Sistema de Trazabilidad. ¡Hasta pronto!")
+        break
+
+    else:
+        print("Opción no válida. Por favor seleccione 1, 2 o 3.")
